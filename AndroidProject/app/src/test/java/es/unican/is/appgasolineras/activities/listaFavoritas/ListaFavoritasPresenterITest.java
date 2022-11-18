f0febf7e7200d7a4b2d69497a9b9ec5b4e67601a
Mario Ingelmo Diana <mid292@alumnos.unican.es>
2022-11-18 18:07:01 +0100

    Rama terminada

--- AndroidProject/app/src/test/java/es/unican/is/appgasolineras/activities/listaFavoritas/ListaFavoritasPresenterITest.java
+++ AndroidProject/app/src/test/java/es/unican/is/appgasolineras/activities/listaFavoritas/ListaFavoritasPresenterITest.java
@@ -176,7 +176,7 @@ public class ListaFavoritasPresenterITest {
         List<Gasolinera> listaFav = new ArrayList<>();
         presenter.init();
         assertEquals(presenter.conseguirGasolinerasActualizadas(false, listaFav, id), repository.getGasolineras("06"));
-/*
+
         //Caso 2
         // MasDeUna = true
         id = "06";
@@ -185,16 +185,16 @@ public class ListaFavoritasPresenterITest {
         g1.setIDCCAA("06");
         g1.setId("1039");
         Gasolinera g2 = new Gasolinera();
-        g2.setIdMun("1000");
+        g2.setIdMun("1100");
         g2.setIDCCAA("09");
         g2.setId("1673");
         listaFav.add(g1);
         listaFav.add(g2);
         List<Gasolinera> listaDef = new ArrayList<>();
-        listaDef.addAll(repository.gasolinerasMunicipio("1"));
-        listaDef.addAll(repository.gasolinerasMunicipio("1000"));
+        listaDef.addAll(repository.gasolinerasMunicipio("1100"));
+        listaDef.addAll(repository.gasolinerasMunicipio("5744"));
         assertEquals(listaDef, presenter.conseguirGasolinerasActualizadas(true, listaFav, id));
-*/
+
         //Caso 3
         //MasDeUna = true
         listaFav.clear();
@@ -206,7 +206,7 @@ public class ListaFavoritasPresenterITest {
         //MasDeUna = false e id = null
         listaFav.clear();
         assertNull(null, presenter.conseguirGasolinerasActualizadas(false, listaFav, null));
-        
+
         dao.deleteAll();
     }
 }
