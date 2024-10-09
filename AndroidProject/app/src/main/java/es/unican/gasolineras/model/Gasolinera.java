66b5c01aa6a1cb346ebe488b32c3a746b315871a
adrianDelRio <45372134+adrianDelRio@users.noreply.github.com>
2024-10-09 17:48:18 +0200

    Added getPrecioGasolina()

--- AndroidProject/app/src/main/java/es/unican/gasolineras/model/Gasolinera.java
+++ AndroidProject/app/src/main/java/es/unican/gasolineras/model/Gasolinera.java
@@ -33,5 +33,29 @@ public class Gasolinera {
 
     @SerializedName("Precio Gasoleo A")             protected double gasoleoA;
     @SerializedName("Precio Gasolina 95 E5")        protected double gasolina95E5;
-
+    
+    /**
+     * Returns the summary price of a gas station.
+     *
+     * The summary price is a weighted average of the gas valid prices of gasoline and diesel, where gasoline has a weight of 2 and diesel has a weight of 1.
+     *
+     * @return the summary price of a gas station.
+     */
+    public double getPrecioGasolina() {
+        double gasolinePrice = 0;
+        double dieselPrice = 0;
+        double gasolineWeight = 0;
+        double dieselWeight = 0;
+        double summaryPrice = 0;
+        if (gasolina95E5 > 0) {
+            gasolinePrice = gasolina95E5;
+            gasolineWeight = 2;
+        }
+        if (gasoleoA > 0) {
+            dieselPrice = gasoleoA;
+            dieselWeight = 1;
+        }
+        summaryPrice = (gasolinePrice * gasolineWeight + dieselPrice * dieselWeight) / (gasolineWeight + dieselWeight);
+        return summaryPrice;
+    }
 }
