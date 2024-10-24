b05b1b520b140da8ef5c7bc4165b7d15f2f636bd
adrianDelRio <45372134+adrianDelRio@users.noreply.github.com>
2024-10-24 23:48:00 +0200

    test: integration with founded bugs

--- AndroidProject/app/src/test/java/es/unican/gasolineras/activities/main/MainPresenterITest.java
+++ AndroidProject/app/src/test/java/es/unican/gasolineras/activities/main/MainPresenterITest.java
@@ -5,8 +5,11 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -37,19 +40,11 @@ public class MainPresenterITest {
 
     private IMainContract.Presenter sut;
 
-    List<Gasolinera> l;
+    @Captor
+    ArgumentCaptor<List<Gasolinera>> listCaptor;
 
-    public void setUp(String[] nombres, Double[] gasolinaG5E5, Double[] gasoleoA) {
-        l = new ArrayList<>();
-        Gasolinera g;
-        for (int i = 0; i<nombres.length; i++) {
-            g = new Gasolinera();
-            g.setRotulo(nombres[i]);
-            g.setGasolina95E5(gasolinaG5E5[i]);
-            g.setGasoleoA(gasoleoA[i]);
-            l.add(g);
-        }
-
+    @Before
+    public void setUp() {
         MockitoAnnotations.openMocks(this);
         when(view.getGasolinerasRepository()).thenReturn(repository);
         sut = new MainPresenter();
@@ -59,10 +54,7 @@ public class MainPresenterITest {
     @Test
     public void onFiltersPopUpAcceptClickedGasolina95E5() {
         // Gasolineras filtradas por Gasolina95E5
-        String[] nombres = {"CEPSA", "REPSOL", "PETRONOR", "PETRONOR V2", "GALP"};
-        Double[] gasolinaG5E5 = {1.679, 1.669, 1.639, 1.639, 1.639};
-        Double[] gasoleoA = {1.509, 1.569, 1.525, 1.525, 0.0};
-        setUp(nombres, gasolinaG5E5, gasoleoA);
+        String[] rotulos = {"CEPSA", "REPSOL", "PETRONOR", "PETRONOR V2", "GALP"};
         sut.setTempFilter(new Filter()
                 .setFuelTypes(Collections.singletonList(FuelTypeEnum.GASOLINA_95E5))
         );
@@ -72,9 +64,11 @@ public class MainPresenterITest {
         // Son 2 veces porque "sut.init(view);" llama al load()
         verify(view, times(2)).getGasolinerasRepository();
         // {CEPSA, REPSOL, PETRONOR, PETRONOR V2, GALP}
-        verify(view).showLoadCorrect(l.size());
-        // FIXME Falla unicamente porque las referencias de los objetos son diferentes, los objetos son iguales
-        verify(view).showStations(l);
+        verify(view).showLoadCorrect(rotulos.length);
+        verify(view, times(2)).showStations(listCaptor.capture());
+        for (int i = 0; i < rotulos.length; i++) {
+            Assert.assertEquals(rotulos[i], listCaptor.getValue().get(i).getRotulo());
+        }
         // Filter = {Gasolina95E5}
         IFilter f = sut.getFilter();
         Assert.assertEquals(1, f.getFuelTypes().size());
@@ -89,10 +83,7 @@ public class MainPresenterITest {
     @Test
     public void onFiltersPopUpAcceptClickedGasoleoA() {
         // Gasolineras filtradas por GasoleoA
-        String[] nombres = {"CEPSA", "REPSOL", "PETRONOR", "PETRONOR V2", "GALP"};
-        Double[] gasolinaG5E5 = {1.679, 1.669, 1.639, 1.639, 1.639};
-        Double[] gasoleoA = {1.509, 1.569, 1.525, 1.525, 0.0};
-        setUp(nombres, gasolinaG5E5, gasoleoA);
+        String[] rotulos = {"CEPSA", "REPSOL", "PETRONOR", "PETRONOR V2", "GALP"};
         sut.setTempFilter(new Filter()
                 .setFuelTypes(Collections.singletonList(FuelTypeEnum.GASOLEO_A))
         );
@@ -102,9 +93,11 @@ public class MainPresenterITest {
         // Son 2 veces porque "sut.init(view);" llama al load()
         verify(view, times(2)).getGasolinerasRepository();
         // {CEPSA, REPSOL, PETRONOR, PETRONOR V2, GALP}
-        verify(view).showLoadCorrect(l.size());
-        // FIXME Falla unicamente porque las referencias de los objetos son diferentes, los objetos son iguales
-        verify(view).showStations(l);
+        verify(view).showLoadCorrect(rotulos.length);
+        verify(view, times(2)).showStations(listCaptor.capture());
+        for (int i = 0; i < rotulos.length; i++) {
+            Assert.assertEquals(rotulos[i], listCaptor.getValue().get(i).getRotulo());
+        }
         // Filter = {GasoleoA}
         IFilter f = sut.getFilter();
         Assert.assertEquals(1, f.getFuelTypes().size());
@@ -119,19 +112,18 @@ public class MainPresenterITest {
     @Test
     public void onFiltersPopUpAcceptClickedGasolina95E5GasoleoA() {
         // Gasolineras filtradas por gasolina95E5 y gasoleoA
-        String[] nombres = {"CEPSA", "REPSOL", "PETRONOR", "PETRONOR V2", "GALP"};
-        Double[] gasolinaG5E5 = {1.679, 1.669, 1.639, 1.639, 1.639};
-        Double[] gasoleoA = {1.509, 1.569, 1.525, 1.525, 0.0};
-        setUp(nombres, gasolinaG5E5, gasoleoA);
+        String[] rotulos = {"CEPSA", "REPSOL", "PETRONOR", "PETRONOR V2", "GALP"};
         sut.setTempFilter(new Filter());
         sut.onFiltersPopUpAcceptClicked();
         verify(view).closeFiltersPopUp();
         // Son 2 veces porque "sut.init(view);" llama al load()
         verify(view, times(2)).getGasolinerasRepository();
         // {CEPSA, REPSOL, PETRONOR, PETRONOR V2, GALP}
-        verify(view).showLoadCorrect(l.size());
-        // FIXME Falla unicamente porque las referencias de los objetos son diferentes, los objetos son iguales
-        verify(view).showStations(l);
+        verify(view).showLoadCorrect(rotulos.length);
+        verify(view, times(2)).showStations(listCaptor.capture());
+        for (int i = 0; i < rotulos.length; i++) {
+            Assert.assertEquals(rotulos[i], listCaptor.getValue().get(i).getRotulo());
+        }
         // Filter = {Gasolina95E5, GasoleoA}
         IFilter f = sut.getFilter();
         Assert.assertNull(f.getFuelTypes());
