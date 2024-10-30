bcbcf1301c910db33a1075b140301fd54c1cd90e
lucia <lfm577@alumnos.unican.es>
2024-10-30 15:13:06 +0100

    Modified in DAO, removing unnecessary methods

--- AndroidProject/app/src/main/java/es/unican/gasolineras/roomDAO/InterestPointsDAO.java
+++ AndroidProject/app/src/main/java/es/unican/gasolineras/roomDAO/InterestPointsDAO.java
@@ -22,13 +22,12 @@ public class InterestPointsDAO  {
 
     private IInterestPointsDAO myInterestPointsDAO;
 
-    protected InterestPointsDAO(Context context) {
+    protected  InterestPointsDAO(Context context) {
         Context appContext = context.getApplicationContext();
         // Create the database with the entity class, without creating a parallel execution thread
         InterestPointsDatabase database = Room.databaseBuilder(appContext, InterestPointsDatabase.class, "interestpoints")
                 .allowMainThreadQueries().build();
         myInterestPointsDAO = database.getInterestPointsDAO();
-
     }
 
     public static InterestPointsDAO getInstance(Context context){
@@ -37,58 +36,6 @@ public class InterestPointsDAO  {
         }
         return myInstance;
     }
-
-    /**
-     * Method to call the @Query method defined in the InterestPointsDAO interface.
-     * @return a list of interest points.
-     */
-
-    public List<InterestPoints> getInterestPoints() {
-        return Collections.emptyList();
-    }
-
-    /**
-     * Method to call the @Query method defined in the InterestPointsDAO interface.
-     * @return an interest point searched by name.
-     */
-
-    public InterestPoints getInterestPointByName(String name) {
-        return myInterestPointsDAO.getInterestPointByName(name);
-    }
-
-    /**
-     * Method to call the @Query method defined in the InterestPointsDAO interface.
-     * @return an interest point searched by id
-     */
-
-    public InterestPoints getInterestPointById(int id) {
-        return myInterestPointsDAO.getInterestPointById(id);
-    }
-
-    /**
-     * Method to call the @insert method defined in the InterestPointsDAO interface.
-     */
-
-    public void addInterestPoint(InterestPoints interestPoint) {
-        myInterestPointsDAO.addInterestPoint(interestPoint);
-    }
-
-    /**
-     * Method to call the @delete method defined in the InterestPointsDAO interface.
-     */
-
-    public void deleteInterestPoint(InterestPoints interestPoint) {
-        myInterestPointsDAO.deleteInterestPoint(interestPoint);
-    }
-
-    /**
-     * Method to call the @update method defined in the InterestPointsDAO interface.
-     */
-
-    public void updateInterestPoint(InterestPoints interestPoint) {
-        myInterestPointsDAO.updateInterestPoint(interestPoint);
-    }
-
 
 
 }
