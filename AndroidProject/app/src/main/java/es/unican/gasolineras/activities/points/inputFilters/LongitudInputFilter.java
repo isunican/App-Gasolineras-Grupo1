a64c01ab8197b2db4336aaa4ddfb7f6bf79e3935
Miguel Monje <mmv776@alumnos.unican.es>
2024-11-06 18:21:09 +0100

    Configurado formato campos de texto y Ajustes en las ventanas

---
+++ AndroidProject/app/src/main/java/es/unican/gasolineras/activities/points/inputFilters/LongitudInputFilter.java
@@ -0,0 +1,30 @@
+package es.unican.gasolineras.activities.points.inputFilters;
+
+import android.text.InputFilter;
+import android.text.Spanned;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LongitudInputFilter implements InputFilter {
+    private Pattern coordPattern;
+
+    public LongitudInputFilter() {
+        coordPattern = Pattern.compile("-?\\d{0,3}(\\.\\d{0,4})?");
+    }
+
+    @Override
+    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+
+        String initial = dest.subSequence(0,dstart).toString();
+        String replacement = source.subSequence(start,end).toString();
+        String finalS = dest.subSequence(dend,(dest.length())).toString();
+        String result = initial +replacement +finalS;
+        Matcher matcher = coordPattern.matcher(result);
+        if (!matcher.matches()) {
+            return "";
+        }
+        return null;
+    }
+}
+
