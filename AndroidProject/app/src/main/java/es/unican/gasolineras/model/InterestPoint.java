ad467a1a1cbedb724d851faa2ba55b5c4e844c2a
Lucia <lfm577@alumnos.unican.es>
2024-11-20 15:07:32 +0100

    Added hashCode in InterestPoint

--- AndroidProject/app/src/main/java/es/unican/gasolineras/model/InterestPoint.java
+++ AndroidProject/app/src/main/java/es/unican/gasolineras/model/InterestPoint.java
@@ -14,6 +14,7 @@ import androidx.room.PrimaryKey;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 public class InterestPoint implements Parcelable {
@@ -267,4 +268,16 @@ public class InterestPoint implements Parcelable {
         }
     };
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InterestPoint that = (InterestPoint) o;
+        return id == that.id && colorArgb == that.colorArgb && Double.compare(latitude, that.latitude) == 0 && Double.compare(longitude, that.longitude) == 0 && Double.compare(radius, that.radius) == 0 && Objects.equals(name, that.name) && Objects.equals(creationDate, that.creationDate) && Objects.equals(color, that.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, colorArgb, latitude, longitude, radius, creationDate, color);
+    }
 }
